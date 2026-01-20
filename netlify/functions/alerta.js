ff6cf3d71db78726286eef03631b6564194243ab
netlify27-dotcom <netlify27@gmail.com>
2026-01-20 18:23:51 -0300

    Update alerta.js

--- netlify/functions/alerta.js
+++ netlify/functions/alerta.js
@@ -1,1 +1,15 @@
+exports.handler = async () => {
+  await fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      chat_id: process.env.CHAT_ID,
+      text: "🚨 ALERTA: CLIENTE DETECTADO"
+    })
+  });
 
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ ok: true })
+  };
+};
